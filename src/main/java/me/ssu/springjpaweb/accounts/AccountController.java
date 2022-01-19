3203f198d9704e32c59c6168684ea555149ec18d
김수환 <zzanggoon8@hiconsy.com>
2022-01-19 21:49:52 +0900

    account custom form validated

--- src/main/java/me/ssu/springjpaweb/accounts/AccountController.java
+++ src/main/java/me/ssu/springjpaweb/accounts/AccountController.java
@@ -1,15 +1,29 @@
 package me.ssu.springjpaweb.accounts;
 
+import lombok.RequiredArgsConstructor;
+import me.ssu.springjpaweb.validators.SignUpFormValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    // TODO 회원가입 폼 커스텀 검증(타입의 이름(signUpForm)을 따라간다)
+    // TODO InitBinder 사용하기(커스텀 검증은 회원 가입 처리할 때 알 수 있음)
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     // TODO 회원가입 폼
     @GetMapping("/sign-up")
@@ -18,12 +32,19 @@ public class AccountController {
         return "accounts/sign-up";
     }
 
-    // TODO 회원가입 폼 서브밋 검증
     @PostMapping("/sign-up")
     public String signUpForm(@Valid SignUpForm signUpForm, Errors errors) {
+
+        // TODO 회원가입 폼 서브밋 검증
         if (errors.hasErrors()) {
             return "accounts/sign-up";
         }
+
+        // TODO 회원가입 폼 커스텀 검증(아래 코드는 InitBinder로 처리하기)
+//        signUpFormValidator.validate(signUpForm, errors);
+//        if (errors.hasErrors()) {
+//            return "accounts/sign-up";
+//        }
         // TODO 회원 가입 처리
         return "redirect:/";
     }
