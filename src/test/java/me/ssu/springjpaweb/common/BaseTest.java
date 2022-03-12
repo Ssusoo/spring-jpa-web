bb5ebe8d6c59687dffdbc98a9aca8673dce7849c
zzanggoon8 <zzanggoon8@gmail.com>
2022-03-12 20:33:02 +0900

    account TDD

--- src/test/java/me/ssu/springjpaweb/common/BaseTest.java
+++ src/test/java/me/ssu/springjpaweb/common/BaseTest.java
@@ -24,7 +24,7 @@ public class BaseTest {
     protected ObjectMapper objectMapper;
 
     @Autowired
-    protected AccountRepository accountRepository;
+    protected AccountRepository accountRepository;g
 
     // TODO
     @MockBean
